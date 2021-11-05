14711849c64e515995b4879fa4ad83fc446a5bb8
ngut37 <ngut37@vse.cz>
2021-11-06 00:43:36 +0100

    fix: 🐛 config process env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,20 @@
 import { createConfig } from './create-config';
 import { Config } from './types';
 
-const defaultConfig: Config = {};
+const defaultConfig: Config = {
+  API_URL: process.env.API_URL,
+};
 
 export const config = createConfig(
   {
-    development: { API_URL: 'http://localhost:4000' },
+    development: {
+      API_URL: 'http://localhost:4000/',
+    },
     preview: {},
     test: {},
-    production: { API_URL: 'https://zohan-services.oa.r.appspot.com/' },
+    production: {
+      API_URL: 'https://zohan-services.oa.r.appspot.com/',
+    },
   },
   defaultConfig,
 );
